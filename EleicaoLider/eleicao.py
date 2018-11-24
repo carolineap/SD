24733a4911987ae0682900a2ebec5403147166ba
Caroline <hicaarol@gmail.com>
2018-11-24 12:02:54 -0200

    commit inicial eleicao

---
+++ EleicaoLider/eleicao.py
@@ -0,0 +1,10 @@
+#Caroline Aparecida de Paula Silva - 726506
+#Isabela Sayuri Matsumoto - 726539
+#Sistemas Distribuidos - Eleicao de Lider - P1
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__": main()
